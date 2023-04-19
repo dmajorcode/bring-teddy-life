422a18af4d2368e232642871b4d78befa2d5d378
dmajorcode <dianekang93@gmail.com>
2023-04-19 18:02:35 +0900

    add: session

--- bring-teddy-life.swiftpm/src/AR/ARTeddyViewController.swift
+++ bring-teddy-life.swiftpm/src/AR/ARTeddyViewController.swift
@@ -8,7 +8,34 @@ import UIKit
 import RealityKit
 import ARKit
 
-class ARTeddyViewController: UIViewController {
+
+class ARTeddyViewControllerSuper: UIViewController {
+    let arView = ARView()
+    
+    var session: ARSession{
+        arView.session
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(arView)
+        arView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        let coachingOverlay = ARCoachingOverlayView()
+        coachingOverlay.session = session
+        coachingOverlay.goal = .horizontalPlane
+        arView.addSubview(coachingOverlay)
+        coachingOverlay.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+}
+
+class ARTeddyViewController: ARTeddyViewControllerSuper {
+//    @IBOutlet var arView: ARView!
+    private var teddyAnchor: AnchorEntity!
+    private var cameraAnchor: AnchorEntity!
 //    @IBOutlet var arView: ARView!
 //
 //    var roboAnchor: RoboBear.Scene!
@@ -24,25 +51,93 @@ class ARTeddyViewController: UIViewController {
 //        roboAnchor.notifications.roboStart.post()
 //    }
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("this is arVIew:",arView)
+        arView.session.delegate = self
+        setupARView()
         
-        let anchor = AnchorEntity()
-        arView.scene.anchors.append(anchor)
-        Experience.loadTeddyAsync(completion: {[weak self](result) in
-            let experience = try! result.get()
-            let bear = experience.group as! (Entity & HasCollision)
-            anchor.addChild(bear)
-            self?.arView.installGestures(for: bear)
-        })
-        
+        arView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
     }
+    func setupARView(){
+        print("in setupARView")
+        arView.automaticallyConfigureSession = false
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+        configuration.environmentTexturing = .automatic
+        arView.session.run(configuration)
+        print("i am all made")
+    }
+    @objc
+    func handleTap(recognizer: UITapGestureRecognizer){
+        print("i am in handle tap")
+        let location = recognizer.location(in: arView)
+        
+        
+        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        if let firstResult = results.first {
+            let anchor = ARAnchor(name: "TeddyBear", transform: firstResult.worldTransform)
+            arView.session.add(anchor: anchor)
+        } else {
+            print("Object placement failed - couldn't find surface.")
+        }
+    }
+    func placeObject(named entityName: String, for anchor: ARAnchor) {
+        print("i am in placing object")
+        let entity = try! ModelEntity.loadModel(named: entityName)
+        
+        entity.generateCollisionShapes(recursive: true)
+        arView.installGestures([.rotation, .translation], for: entity)
+        
+        let anchorEntity = AnchorEntity(anchor: anchor)
+        anchorEntity.addChild(entity)
+        arView.scene.addAnchor(anchorEntity)
+    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        initAnchors()
+//
+//        Experience.loadTeddyAsync{[weak self] result in
+//            let experience = try! result.get()
+//            self?.addExperience(experience)
+//        }
+//
+////        let anchor = AnchorEntity()
+////        arView.scene.anchors.append(anchor)
+////        Experience.loadTeddyAsync(completion: {[weak self](result) in
+////            let experience = try! result.get()
+////            let bear = experience.group as! (Entity & HasCollision)
+////            anchor.addChild(bear)
+////            self?.arView.installGestures(for: bear)
+////        })
+//
+//    }
+//    private func addExperience(_ experience: Experience.Teddy){
+//
+//    }
+//    private func initAnchors(){
+//        teddyAnchor = AnchorEntity()
+//        arView.scene.anchors.append(teddyAnchor)
+//
+//        cameraAnchor = AnchorEntity(.camera)
+//        arView.scene.addAnchor(cameraAnchor)
+//    }
     
-    let arView = ARView()
+//    let arView = ARView()
+//
+//    var session: ARSession{
+//        arView.session
+//    }
+}
 
-    var session: ARSession{
-        arView.session
+extension ARTeddyViewController: ARSessionDelegate {
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        for anchor in anchors {
+            if let anchorName = anchor.name, anchorName == "TeddyBear"{
+                placeObject(named: anchorName, for:anchor)
+            }
+        }
     }
 }
--- bring-teddy-life.swiftpm/src/AR/Utils/Reality.swift
+++ bring-teddy-life.swiftpm/src/AR/Utils/Reality.swift
@@ -10,42 +10,54 @@ import RealityKit
 import simd
 import Combine
 
-@available(macOS 13.3.1, *)
+@available(iOS 13.0, macOS 10.15, *)
 public enum Experience {
+
     public enum LoadRealityFileError: Error {
         case fileNotFound(String)
     }
-    
+
     private static var streams = [Combine.AnyCancellable]()
-    
-    public static func loadTeddy() throws -> Experience.Teddy{
-        guard let realityFileURL = Foundation.Bundle(for: Experience.Teddy.self).url(forResource:"Experience", withExtension: "reality") else{
-            throw
-            Experience.LoadRealityFileError.fileNotFound("Experience.reality")
+
+    public static func loadTeddy() throws -> Experience.Teddy {
+        guard let realityFileURL = Foundation.Bundle(for: Experience.Teddy.self).url(forResource: "Experience", withExtension: "reality") else {
+            throw Experience.LoadRealityFileError.fileNotFound("Experience.reality")
         }
-        
+
         let realityFileSceneURL = realityFileURL.appendingPathComponent("Teddy", isDirectory: false)
         let anchorEntity = try Experience.Teddy.loadAnchor(contentsOf: realityFileSceneURL)
         return createTeddy(from: anchorEntity)
     }
-    
-    public static func loadTeddyAsync(completion: @escaping(Swift.Result<Experience.Teddy, Swift.Error>) -> Void){
-        guard let realityFileURL = Foundation.Bundle(for:Experience.Teddy.self).url(forResource: "Experience", withExtension: "reality") else{completion(.failure(Experience.LoadRealityFileError.fileNotFound("Experience.reality")))
+
+    public static func loadTeddyAsync(completion: @escaping (Swift.Result<Experience.Teddy, Swift.Error>) -> Void) {
+        guard let realityFileURL = Foundation.Bundle(for: Experience.Teddy.self).url(forResource: "Experience", withExtension: "reality") else {
+            completion(.failure(Experience.LoadRealityFileError.fileNotFound("Experience.reality")))
             return
         }
+
+        var cancellable: Combine.AnyCancellable?
+        let realityFileSceneURL = realityFileURL.appendingPathComponent("Teddy", isDirectory: false)
+        let loadRequest = Experience.Teddy.loadAnchorAsync(contentsOf: realityFileSceneURL)
+        cancellable = loadRequest.sink(receiveCompletion: { loadCompletion in
+            if case let .failure(error) = loadCompletion {
+                completion(.failure(error))
+            }
+            streams.removeAll { $0 === cancellable }
+        }, receiveValue: { entity in
+            completion(.success(Experience.createTeddy(from: entity)))
+        })
+        cancellable?.store(in: &streams)
     }
-    private static func createTeddy(from anchorEntity: RealityKit.AnchorEntity) -> Experience.Teddy{
+
+    private static func createTeddy(from anchorEntity: RealityKit.AnchorEntity) -> Experience.Teddy {
         let teddy = Experience.Teddy()
         teddy.anchoring = anchorEntity.anchoring
         teddy.addChild(anchorEntity)
         return teddy
     }
-    
+
     public class Teddy: RealityKit.Entity, RealityKit.HasAnchoring {
-        public var group: RealityKit.Entity?{
-            return self.findEntity(named: "group")
-        }
+
     }
+
 }
-
-
